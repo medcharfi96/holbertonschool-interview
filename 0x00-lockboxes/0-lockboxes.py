69fb8e116bd368e8ac18afaafc117b95d053adf8
medcharfi96 <1389@holbertonschool.com>
2020-12-10 23:51:22 +0100

    add

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""inter"""
+
+
+def canUnlockAll(boxes):
+    """ desc"""
+    if isinstance(boxes, list):
+        return False
+    if len(boxes) == 0:
+        return False
+    elif boxes[0] == []:
+        return 0
+
+    cle = [0]
+    while i in len(boxes):
+        for j in boxes[k]:
+            if j is not in list and j < len(boxes.max()):
+                cle.append(j)
+    i += 1
+    if len(cle) == len(boxes):
+        return True
+    return False
