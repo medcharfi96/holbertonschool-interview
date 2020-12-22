5425ae27a8bcae426a4950d8cdbb732033e9d4ea
medcharfi96 <1389@holbertonschool.com>
2020-12-22 22:24:18 +0100

    add

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -13,9 +13,9 @@
 typedef struct binary_tree_s
 {
 int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 } binary_tree_t;
 typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *tree);
