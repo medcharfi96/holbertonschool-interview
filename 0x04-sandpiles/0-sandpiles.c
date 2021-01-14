7fecc14bc7f2eca01b7a5340d1807f5a60d619d3
medcharfi96 <1389@holbertonschool.com>
2021-01-14 23:09:35 +0100

    add

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -8,18 +8,18 @@
  */
 int chek_grid(int grid1[3][3])
 {
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			if (grid1[i][j] > 3)
-			{
-				return (0);
-			}
+for (int i = 0; i < 3; i++)
+{
+for (int j = 0; j < 3; j++)
+{
+if (grid1[i][j] > 3)
+{
+return (0);
+}
 
-		}
-	}
-	return (1);
+}
+}
+return (1);
 }
 /**
  *sallah_grid - fonction de rectification de grid
@@ -28,27 +28,26 @@ int chek_grid(int grid1[3][3])
  */
 void sallah_grid(int grid1[3][3])
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			if (grid1[i][j] > 3)
-			{
-				grid1[i][j] = grid1[i][j] - 4;
-				if (j + 1 < 3)
-					grid1[i][j + 1] = grid1[i][j + 1] + 1;
-				if (j - 1 > -1)
-					grid1[i][j - 1] = grid1[i][j - 1] + 1;
-				if (i + 1 < 3)
-					grid1[i + 1][j] = grid1[i + 1][j] + 1;
-				if (i - 1 > -1)
-					grid1[i - 1][j] = grid1[i - 1][j] + 1;
-			}
-		}
-
-	}
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (grid1[i][j] > 3)
+{
+grid1[i][j] = grid1[i][j] - 4;
+if (j + 1 < 3)
+grid1[i][j + 1] = grid1[i][j + 1] + 1;
+if (j - 1 > -1)
+grid1[i][j - 1] = grid1[i][j - 1] + 1;
+if (i + 1 < 3)
+grid1[i + 1][j] = grid1[i + 1][j] + 1;
+if (i - 1 > -1)
+grid1[i - 1][j] = grid1[i - 1][j] + 1;
+}
+}
+}
 }
 /**
  *sandpiles_sum - main function
@@ -58,32 +57,28 @@ void sallah_grid(int grid1[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j;
+int i, j;
 
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			grid1[i][j] = grid2[i][j] + grid1[i][j];
-		}
-
-	}
-
-
-	while (!chek_grid(grid1))
-	{
-		printf("=\n");
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				if (j)
-					printf(" ");
-				printf("%d", grid1[i][j]);
-		}
-		printf("\n");
-	}
-		sallah_grid(grid1);
-
-	}
+for (int i = 0; i < 3; i++)
+{
+for (int j = 0; j < 3; j++)
+{
+grid1[i][j] = grid2[i][j] + grid1[i][j];
+}
+}
+while (!chek_grid(grid1))
+{
+printf("=\n");
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (j)
+printf(" ");
+printf("%d", grid1[i][j]);
+}
+printf("\n");
+}
+sallah_grid(grid1);
+}
 }
