8e7e2a313e2b3707d282a20fe38afec38b01aecb
medcharfi96 <1389@holbertonschool.com>
2021-01-14 22:59:59 +0100

    add

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,8 +1,11 @@
 #ifndef _sandpiles_H_
 #define _sandpiles_H_
+
 #include <stdio.h>
 #include <stdlib.h>
+
 int chek_grid(int grid1[3][3]);
 void sallah_grid(int grid1[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
 #endif
