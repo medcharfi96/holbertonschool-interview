3f84a878d557e902c1048e0861e53d9dc3068b05
medcharfi96 <1389@holbertonschool.com>
2021-01-14 22:46:28 +0100

    add

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -36,11 +36,12 @@ void sallah_grid(int grid1[3][3])
 		{
 			if (grid1[i][j] > 3)
 			{
+				grid1[i][j] = grid1[i][j] - 4;
 				if (j + 1 < 3)
 					grid1[i][j + 1] = grid1[i][j + 1] + 1;
 				if (j - 1 > -1)
-					grid1[i][j - 1] = grid1[i][j - 1] + 1
-				grid1[i][j] = grid1[i][j] - 4;
+					grid1[i][j - 1] = grid1[i][j - 1] + 1;
+
 				if (i + 1 < 3)
 					grid1[i + 1][j] = grid1[i + 1][j] + 1;
 				if (i - 1 > -1)
