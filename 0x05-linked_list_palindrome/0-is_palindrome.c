e797b18b9c4b495ba3a53ebb33ed631c134dfb0f
medcharfi96 <1389@holbertonschool.com>
2021-01-20 15:36:38 +0100

    add

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -5,13 +5,14 @@
  * longueur_list - give the list lenght
  * @p: copy of head
  * Return: int(mean true)
- * 
+ *
  */
 int longueur_list(listint_t **p)
 {
 	int i = 1;
 	listint_t *pd = *p;
-	while(pd->next != NULL)
+
+	while (pd->next != NULL)
 	{
 		i++;
 		pd = pd->next;
@@ -19,26 +20,6 @@ int longueur_list(listint_t **p)
 	return (i);
 }
 
-/**
- * copy_list - copy list in a array
- * @d: pointer to head of list
- * @len: lenght of list
- * Return: tab(mean true)
- */
-
-int * copy_list(listint_t **d, int len)
-{
-	listint_t *ras = *d;
-	int i = 0;
-	int arry[len];
-
-	while(i < len)
-	{
-		arry[i] = ras->n;
-		ras = ras->next;
-	}
-	return (arry);
-}
 /**
  * s_palindrome - check  if is palindrome
  * @head: pointer to head of list
@@ -48,6 +29,7 @@ int * copy_list(listint_t **d, int len)
 int is_palindrome(listint_t **head)
 {
 	int i = 0;
+	int j = 0;
 	int len = longueur_list(head);
 	int tab[len];
 	int bol = 1;
@@ -56,10 +38,15 @@ int is_palindrome(listint_t **head)
 	{
 		return (0);
 	}
-	tab = copy_list(head, len);
-	while (i <= len/2 && bol == 1)
+	while (j < len)
 	{
-		if (tab[i] != tab[len - i - 1])
+		tab[j] = (*head)->n;
+		*head = (*head)->next;
+		j++;
+	}
+	while ((i <= len / 2) && bol == 1)
+	{
+		if (tab[len - i - 1] != tab[i])
 		{
 			bol = 0;
 		}
