4da524244c30b0f355fddb5e162842abb9c7767e
medcharfi96 <1389@holbertonschool.com>
2021-01-31 14:56:13 +0100

    add

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,11 +10,12 @@ tll = 0
 try:
     for x, line in enumerate(sys.stdin, 1):
         s_l = line.split()
+		s_l = s_l[::-1]
         if len(s_l) > 2:
-            stat_c = s_l[len(s_l) - 2]
+            stat_c = s_l[0]
             tll += int(s_l[len(s_l) - 1])
-            if stat_c in codet:
-                codet[str(stat_c)] += 1
+            if stat_c in codet.keys():
+                codet[stat_c] += 1
         if x == 10:
             print("File size: {}".format(tll))
             for k, val in sorted(codet):
