840429590375b9eb7fa4096013210f81de4f13b1
medcharfi96 <1389@holbertonschool.com>
2021-01-31 14:50:24 +0100

    add

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,30 +5,25 @@ import sys
 
 codet = {'200': 0, '301': 0, '400': 0, '401': 0,
          '403': 0, '404': 0, '405': 0, '500': 0}
-compteur = 0
 tll = 0
 
 try:
-    for l in sys.stdin:
-        data = l.split()
-        data = data[::1]
-        if len(data) > 2:
-            compteur += 1
-            if compteur <= 10:
-                tll += int(data[0])
-                ras_zeb = data[1]
-            if ras_zeb in codet.keys():
-                codet[ras_zeb] += 1
-            if compteur % 10 == 0:
-                print('File size: {}'.format(tll))
-                for num_err, mch in sorted(codet.items()):
-                    if mch > 0:
-                        print('{}: {}'.format(num_err, mch))
-                counter = 0
+    for x, line in enumerate(sys.stdin, 1):
+        s_l = line.split()
+        if len(s_l) > 2:
+            statuscode = s_l[len(s_l) - 2]
+            tll += int(s_l[len(s_l) - 1])
+            if statuscode in codet:
+                codet[statuscode] += 1
+        if x == 10:
+            print("File size: {}".format(tll))
+            for k, val in sorted(codet):
+                if val > 0:
+                    print("{}: {}".format(k, val))
 except KeyboardInterrupt:
     pass
 finally:
-    print('File size: {}'.format(tll))
-    for num_err, mch in sorted(codet.items()):
-        if mch > 0:
-            print('{}: {}'.format(num_err, mch))
+    print('File size: {:d}'.format(tll))
+    for k, val in sorted(codet.keys()):
+        if val != 0:
+            print("{}: {:d}".format(k, val))
