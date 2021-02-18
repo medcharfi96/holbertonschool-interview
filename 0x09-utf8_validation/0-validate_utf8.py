43cd93bba3a78a39a09c12a1400174acd9db6c8a
medcharfi96 <1389@holbertonschool.com>
2021-02-18 22:38:04 +0100

    add

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" task 0"""
+
+
+def validUTF8(data):
+    """
+    :type data: List[int]
+    :rtype: bool
+    """
+    nbr_b = 0
+    test1 = 1 << 7
+    test2 = 1 << 6
+    for count in data:
+        mask = 1 << 7
+        if nbr_b == 0:
+            while mask & count:
+                nbr_b = nbr_b + 1
+                mask = mask >> 1
+            if nbr_b == 1 or nbr_b > 4:
+                return False
+            if nbr_b == 0:
+                continue
+        else:
+            if (count >> 6) != 0b10:
+                return False
+        nbr_b = nbr_b - 1
+    if nbr_b == 0:
+        return True
+    return False
