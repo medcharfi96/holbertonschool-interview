79c233b4b9577f33e2676d7ef2c590df5d634ed7
medcharfi96 <1389@holbertonschool.com>
2021-06-10 16:59:25 +0100

    add

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" task """
+""" task 0 """
+
+
 from operator import itemgetter
 import requests
 
 
 def count_words(subreddit, word_list, hot_list=[], init=0, after="null"):
     """
-	description
-	"""
+    description
+    """
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     agent = {"User-Agent": "linux:1:v2.1 (by /u/heimer_r)"}
@@ -15,35 +17,36 @@ def count_words(subreddit, word_list, hot_list=[], init=0, after="null"):
     hot = requests.get(url, headers=agent,
                        params=payload, allow_redirects=False)
 
-    mot = {}
-    titre = []
-    nbr = {}
-    for kelma in word_list:
-        if kelma not in nbr:
-            nbr[kelma] = 0
     if hot.status_code == 200:
-        fils = hot.json().get('data').get('children')
-        for j in fils:
-            titre.append(j.get('data').get('title'))
-        for title in titre:
-            for key, valeur in nbr.items():
-                copier = title[:]
-                couper = copier.lower().split(key.lower())
-                nbr[key] =  nbr[key]  + (len(couper) - 1)
-        fnl = {}
-        for k in nbr:
-            if  k.lower() in fnl and nbr[k] != 0:
-                fnl[k.lower()] += nbr[k]
-            elif nbr[k] !=0  and  k.lower() not in fnl :
-                fnl[k.lower()] = nbr[k]
-            else:
-                pass
-        val_tri = sorted(fnl.values(), reverse=True)
-        resultat = {}
-
-        for i in val_tri:
-            for j in fnl.keys():
-                if fnl[j] == i:
-                    resultat[j] = fnl[j]
-        for i in resultat.keys():
-            print("{}: {}".format(i, resultat[i]))
+        pub = hot.json().get("data").get("children")
+        hot_list += [ART.get("data").get("title") for ART in pub]
+        after = hot.json().get("data").get("after")
+        if after is not None:
+            count_words(subreddit, word_list, hot_list, 1, after)
+        if init == 0:
+            hot_str = " ".join(hot_list)
+            word_spt = hot_str.split(" ")
+            tab1 = sorted(word_list)
+            retour = []
+            for w in tab1:
+                numero = len(
+                    list(
+                        filter(
+                            lambda hot_w: hot_w.lower() == w.lower(),
+                            word_spt)))
+                if numero != 0:
+                    retour.append([word, numero])
+            if len(retour) != 0:
+                i = 0
+                while i < len(retour) - 1:
+                    if retour[i + 1][0] is not None and
+                    retour[i][0] == retour[i + 1][0]:
+                        retour[i][1] += retour[i + 1][1]
+                        retour.pop(i + 1)
+                        retour.append([None, None])
+                        i -= 1
+                    i += 1
+                r = list(filter(lambda x: x != [None, None], retour))
+                r_sorted = sorted(r, key=lambda x: (x[1]), reverse=True)
+                for i in r_sorted:
+                    print(*i, sep=": ")
