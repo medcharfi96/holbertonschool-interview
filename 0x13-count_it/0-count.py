9cf4531b16466cafae68e8567efe0446b93e834c
medcharfi96 <1389@holbertonschool.com>
2021-06-10 23:14:44 +0100

    add

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,52 +1,40 @@
 #!/usr/bin/python3
-""" task 0 """
+""" task 0q"""
 
 
-from operator import itemgetter
 import requests
 
 
-def count_words(subreddit, word_list, hot_list=[], init=0, after="null"):
-    """
-    description
-    """
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    agent = {"User-Agent": "linux:1:v2.1 (by /u/heimer_r)"}
-    payload = {"limit": "100", "after": after}
-    hot = requests.get(url, headers=agent,
-                       params=payload, allow_redirects=False)
-
-    if hot.status_code == 200:
-        pub = hot.json().get("data").get("children")
-        hot_list += [ART.get("data").get("title") for ART in pub]
-        after = hot.json().get("data").get("after")
-        if after is not None:
-            count_words(subreddit, word_list, hot_list, 1, after)
-        if init == 0:
-            hot_str = " ".join(hot_list)
-            word_spt = hot_str.split(" ")
-            tab1 = sorted(word_list)
-            retour = []
-            for w in tab1:
-                numero = len(
-                    list(
-                        filter(
-                            lambda hot_w: hot_w.lower() == w.lower(),
-                            word_spt)))
-                if numero != 0:
-                    retour.append([word, numero])
-            if len(retour) != 0:
-                i = 0
-                while i < len(retour) - 1:
-                    if retour[i + 1][0] is not \
-                            None and retour[i][0] == retour[i + 1][0]:
-                        retour[i][1] += retour[i + 1][1]
-                        retour.pop(i + 1)
-                        retour.append([None, None])
-                        i -= 1
-                    i += 1
-                r = list(filter(lambda x: x != [None, None], retour))
-                r_sorted = sorted(r, key=lambda x: (x[1]), reverse=True)
-                for i in r_sorted:
-                    print(*i, sep=": ")
+def count_words(subreddit, list, after="", count={}):
+    """recursive function that queries the Reddit API"""
+    if after is None:
+        return []
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                AppleWebKit/537.36 (KHTML, like Gecko)Chrome/70.0.3538.7\
+                7 Safari/537.36'}
+    if after:
+        url += '?after={}'.format(after)
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if str(r) != '<Response [200]>':
+        return None
+    data = r.json().get('data')
+    after = data.get('after')
+    list_underlist = data.get('children')
+    for under_list in list_underlist:
+        title = under_list.get('data').get('title')
+        for mot in list:
+            occ = title.lower().split().count(mot.lower())
+            if occ > 0:
+                if mot in count:
+                    count[mot] += occ
+                else:
+                    count[mot] = occ
+    if after is None:
+        if not len(count) > 0:
+            return
+        it = sorted(count.items(), key=lambda kv: (-kv[1], kv[0]))
+        for key, value in it:
+            print('{}: {}'.format(key.lower(), value))
+    else:
+        return count_words(subreddit, list, after, count)
