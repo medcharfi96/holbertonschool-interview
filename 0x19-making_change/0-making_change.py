f8ab53a18e749d69e8501a96c4c6d4e69548314e
medcharfi96 <1389@holbertonschool.com>
2021-06-23 15:12:43 +0100

    add

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" task 0 """
+
+
+def makeChange(coins, total):
+    """"""
+    if (total < 0 or type(total) is not int):
+        return (-1)
+    coin = []
+    coin = coins
+    coin.sort()
+    coin.reverse()
+    op = 0
+    for j in coin:
+        if int(total / j) > 0:
+            op += int(total / j)
+            total -= j * op
+
+        if total == 0:
+            return op
+    return -1
---
+++ 0x19-making_change/README.md
@@ -0,0 +1,5 @@
+# 0x19-making_change
+
+## Holberton school  interview preperation
+
+### Done by : Mouhamed Charfi
