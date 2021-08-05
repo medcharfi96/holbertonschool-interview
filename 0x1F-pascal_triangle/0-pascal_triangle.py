4e010ece3638269ec2faa2981ae75aac516c7a21
medcharfi96 <1389@holbertonschool.com>
2021-08-05 16:22:09 +0100

    add

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""
-Pascal Triangle
-"""
+""" task 0 """
 
 
 def pascal_triangle(n):
     """
-    Function that returns a list of lists of integers
-    representing the Pascal’s triangle of n
-    Arguments:
-     - n will be always an integer
-    Returns:
-     An empty list if n <= 0
+    pascale triangle algoritheme
+    :param n: nbr de pas
     """
 
     tab_F = []
